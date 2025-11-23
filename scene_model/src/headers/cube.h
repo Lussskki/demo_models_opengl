7ad5f1bd1594fc4ba13681f4343bdb1fab05ae3a
Luka <lukaguledani123@gmail.com>
2025-11-23 17:49:00 +0400

    extracted from main cube code

---
+++ scene_model/src/headers/cube.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <GL/glew.h>
+#include <glm/glm.hpp>
+
+struct Cube
+{
+    GLuint VAO, VBO;
+    glm::vec3 position;
+
+    Cube(const glm::vec3& pos = glm::vec3(0.0f));
+    ~Cube();
+
+    void Draw(GLuint shaderProgram, GLint locModel, GLint locUseTex);
+};
+
