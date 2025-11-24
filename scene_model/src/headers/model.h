847bad00d8277bd1ecf5498e04788f54a99684bc
Luka <lukaguledani123@gmail.com>
2025-11-24 14:04:17 +0400

    vertex struct

--- scene_model/src/headers/model.h
+++ scene_model/src/headers/model.h
@@ -4,9 +4,11 @@
 #include <string>
 #include <vector>
 
-struct Vertex {
+struct Vertex
+{
     glm::vec3 Position;
-    glm::vec3 Color; // optional if MTL has color
+    glm::vec3 Normal;
+    glm::vec3 Color;
     glm::vec2 TexCoords;
 };
 
